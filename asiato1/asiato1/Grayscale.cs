c17844169f2059ed37364647917b73ae968bf7f0
nsgt <13ec075@ms.dendai.ac.jp>
2016-07-19 16:10:57 +0900

    Add files via upload

---
+++ asiato1/asiato1/Grayscale.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Drawing.Imaging;
+
+namespace asiato1
+{
+    class Grayscale
+    {
+        public Bitmap CreateGrayscaleImage(Bitmap img)
+        {
+            //グレースケールの描画先となるImageオブジェクトを作成
+            Bitmap newImg = new Bitmap(img.Width, img.Height);           // (256,256)
+            //newImgのGraphicsオブジェクトを取得
+            Graphics g = Graphics.FromImage(newImg);
+
+            //ColorMatrixオブジェクトの作成
+            //グレースケールに変換するための行列を指定する
+            System.Drawing.Imaging.ColorMatrix cm =
+                new System.Drawing.Imaging.ColorMatrix(
+                    new float[][]{
+                new float[]{0.299f, 0.299f, 0.299f, 0 ,0},
+                new float[]{0.587f, 0.587f, 0.587f, 0, 0},
+                new float[]{0.114f, 0.114f, 0.114f, 0, 0},
+                new float[]{0, 0, 0, 1, 0},
+                new float[]{0, 0, 0, 0, 1}
+                    });
+            //ImageAttributesオブジェクトの作成
+            System.Drawing.Imaging.ImageAttributes ia =
+                new System.Drawing.Imaging.ImageAttributes();
+            //ColorMatrixを設定する
+            ia.SetColorMatrix(cm);
+
+            //ImageAttributesを使用してグレースケールを描画
+            g.DrawImage(img,
+                new Rectangle(0, 0, img.Width, img.Height),
+                0, 0, img.Width, img.Height, GraphicsUnit.Pixel, ia);
+
+            //リソースを解放する
+            g.Dispose();
+
+            return newImg;
+        }
+
+    }
+}
